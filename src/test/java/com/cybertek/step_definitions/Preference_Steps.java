5246a344bbdebd2c8cab6968ba86931005050df6
Stanislav <kisievstanislav@gmail.com>
2019-02-20 04:22:38 -0500

    second preferences commit

--- src/test/java/com/cybertek/step_definitions/Preference_Steps.java
+++ src/test/java/com/cybertek/step_definitions/Preference_Steps.java
@@ -2,6 +2,8 @@ package com.cybertek.step_definitions;
 
 
 import com.cybertek.pages.PreferencePage;
+import com.cybertek.utilities.BrowserUtils;
+import com.cybertek.utilities.Driver;
 import cucumber.api.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
@@ -14,6 +16,7 @@ import static com.cybertek.utilities.ApplicationConstants.PreferencesTitle;
 public class Preference_Steps {
 
     PreferencePage preferencePage = new PreferencePage();
+
 
     @Then("manager clicks on Preferences functionality")
     public void manager_clicks_on_Preferences_functionality() {
@@ -33,6 +36,81 @@ public class Preference_Steps {
             System.out.println(down.getText());
         }
     }
+
+    @Then("manger clicks on Preferences functionality and verify Language functionality")
+    public void manger_clicks_on_Preferences_functionality_and_verify_Language_functionality() {
+
+        preferencePage.open();
+        preferencePage.language.getText();
+        Select select = new Select(preferencePage.language);//step4
+        List<WebElement> drop = select.getOptions();//step5
+        for (WebElement down : drop) {
+            System.out.println(down.getText());
+
+        }
+    }
+
+    @Then("manger clicks on Preferences functionality")
+    public void manger_clicks_on_Preferences_functionality() {
+        preferencePage.open();
+    }
+
+    @Then("verify Notification Management functionality")
+    public void verify_Notification_Management_functionality() {
+        String notif = preferencePage.NotifManag.getText();
+        Assert.assertEquals(notif, "Notification Management");//step4
+        preferencePage.NotifManag.click();//step5
+    }
+
+    @Then("verify  Signature functionality")
+    public void verify_Signature_functionality() {
+        Assert.assertEquals(preferencePage.signature.getText(),"Signature");
+
+    }
+
+    @Then("verify Forum Karma functionality")
+    public void verify_Forum_Karma_functionality() {
+        Assert.assertEquals(preferencePage.forumKarma.getText(),"Forum Karma");
+
+    }
+
+    @Then("verify Change password functionality")
+    public void verify_Change_password_functionality() {
+
+        preferencePage.changePassword.click();
+        BrowserUtils.wait(10);
+        Assert.assertEquals(Driver.getDriver().getTitle(),"Change Password - Odoo");
+    }
+
+    @Then("verify  Cancel button and clickable")
+    public void verify_Cancel_button_and_clickable() {
+        preferencePage.changePassword.click();
+        BrowserUtils.wait(10);
+        Assert.assertEquals(preferencePage.cancel.getText(),"Cancel");
+        preferencePage.cancel.click();
+
+    }
+
+
+    @Then("verifying Save button and clickable")
+    public void verifying_Save_button_and_clickable() {
+        String save = preferencePage.SaveButton.getText();
+        Assert.assertEquals(save, "Save");
+
+        preferencePage.SaveButton.click();
+    }
+
+    @Then("verifying cancel button and clickable")
+    public void verifying_cancel_button_and_clickable() {
+        String cancel = preferencePage.CancelButton.getText();
+        Assert.assertEquals(cancel, "Cancel");
+        preferencePage.CancelButton.click();
+    }
+
+
+
+
+
 
 
 
--- src/test/resources/features/preference.feature
+++ src/test/resources/features/preference.feature
@@ -2,16 +2,81 @@ Feature:Preferences functionality
   As a user/manager, when I enter correct login information,
   I should be able to see Preferences Changes
 
-  Scenario: User should be able Verify Change Preferences functionality
+  Scenario: Manager/User should be able verify Change Preferences functionality
     Given manager is on home page
     When manager clicks on Manufacturing module
     Then manager clicks on Preferences functionality
 
-    @wip
-    Scenario: User should be able Verify TimeZone functionality
+
+  Scenario: Manager/User should be able verify TimeZone functionality
       Given manager is on home page
       When manager clicks on Manufacturing module
       Then manager clicks on Preferences functionality and Verify TimeZone
+
+  Scenario: Manager/User should be able verify Language functionality
+    Given manager is on home page
+    When manager clicks on Manufacturing module
+    Then manger clicks on Preferences functionality and verify Language functionality
+
+
+  Scenario: Manager/User should be able verify Notification Management functionality
+    Given manager is on home page
+    When manager clicks on Manufacturing module
+    Then manger clicks on Preferences functionality
+    And verify Notification Management functionality
+
+
+  Scenario: Manager/User should be able verify  Signature functionality
+    Given manager is on home page
+    When manager clicks on Manufacturing module
+    Then manger clicks on Preferences functionality
+    And verify  Signature functionality
+
+
+  Scenario: Manager/User should be able verify "Forum Karma" functionality
+    Given manager is on home page
+    When manager clicks on Manufacturing module
+    Then manger clicks on Preferences functionality
+    And verify Forum Karma functionality
+
+  Scenario: Manager/User should be able verify "Change password" functionality
+    Given manager is on home page
+    When manager clicks on Manufacturing module
+    Then manger clicks on Preferences functionality
+    And verify Change password functionality
+
+
+  Scenario: Manager/User should be able verify  Cancel button
+    Given manager is on home page
+    When manager clicks on Manufacturing module
+    Then manger clicks on Preferences functionality
+    And verify  Cancel button and clickable
+
+
+  Scenario: Manager/User should be able verify  Save button
+    Given manager is on home page
+    When manager clicks on Manufacturing module
+    Then manger clicks on Preferences functionality
+    And verifying Save button and clickable
+
+  @wip
+  Scenario: Manager/User should be able verify  Main Cancel button
+    Given manager is on home page
+    When manager clicks on Manufacturing module
+    Then manger clicks on Preferences functionality
+    And verifying cancel button and clickable
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
